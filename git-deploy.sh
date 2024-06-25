bac42f99f4b125e98d037cb5a179095926396f2c
Alexander Maximovich <75991343+AlexanderMaximovich-azur@users.noreply.github.com>
2024-06-25 16:25:50 +0300

    deploy commit

---
+++ git-deploy.sh
@@ -0,0 +1,22 @@
+sourcePath=$1
+destinationPath=$2
+
+echo $sourcePath
+echo $destinationPath;
+cd $sourcePath
+
+# shellcheck disable=SC2045
+for entry in $(ls $sourcePath)
+do
+  cp $sourcePath"/"$entry $destinationPath"/"$entry
+  echo $entry
+done
+
+
+cd $destinationPath
+git status
+git add -A
+git commit -m "deploy commit"
+git push origin main
+
+
